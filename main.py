cab868091a4e1f7bc03c30e8e9ef8cd41e515c91
Pahan4ik666 <pahashanshin1997@gmail.com>
2024-12-24 21:32:29 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-
-print("(12 + 89 - 1)")
-"1st program"
-print(9 ** 0.5 * 5)
-"2nd program"
-# True Falsse
-print(99 < 99.8)
-print(1000 != 1000.1)
-"3rd program"
-print(2 * 2 + 2 == 2 * (2 + 2))
-"4th program"
-print( 123.456 * 10)
+print("Hello world")
